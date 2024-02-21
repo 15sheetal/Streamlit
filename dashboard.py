9d3822b8b07317c6c321cfc728d224dc212b7259
15sheetal <159413518+15sheetal@users.noreply.github.com>
2024-02-22 00:40:35 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -61,9 +61,6 @@ if selected=="Charts":
         st.plotly_chart(fig2)
     col1,col2=st.columns(2)
     with col1:
-        st.subheader(':green[Donut Chart of Subscription Status]')
-        fig2 = px.pie(df, names='Subscription Status',width=300,hole=0.5,color_discrete_sequence=px.colors.qualitative.Set2)
-        st.plotly_chart(fig2)
         st.subheader(':green[Pie-chart of Categories of Shopping]')
         fig1 = px.pie(names=df['Category'].value_counts().index, values=df['Category'].value_counts().values,
                        color_discrete_sequence=px.colors.qualitative.Pastel,width=250)
