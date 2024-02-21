a2879c4c248c1625cc6ab07f864a5325a1b78d0b
15sheetal <159413518+15sheetal@users.noreply.github.com>
2024-02-21 23:57:51 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -60,16 +60,16 @@ if selected=="Charts":
         fig1 = px.pie(names=df['Category'].value_counts().index, values=df['Category'].value_counts().values,
                        color_discrete_sequence=px.colors.qualitative.Pastel,width=350)
         st.plotly_chart(fig1, use_container_width=True)
-     col1,col2=st.columns(2)
-     with col1:
+    col1,col2=st.columns(2)
+    with col1:
         st.subheader(':green[Donut Chart of Subscription Status]')
         fig2 = px.pie(df, names='Subscription Status',width=300,hole=0.5,color_discrete_sequence=px.colors.qualitative.Set2)
         st.plotly_chart(fig2)
-     with col2:
-       st.subheader(':green[Size Bought by Different Gender]')
-       fig = px.bar(df, x='Size', y='Purchase Amount (USD)',
+    with col2:
+        st.subheader(':green[Size Bought by Different Gender]')
+        fig = px.bar(df, x='Size', y='Purchase Amount (USD)',
                  color='Gender',width=400)
-       st.plotly_chart(fig)
+        st.plotly_chart(fig)
     if st.checkbox('Location'): 
         selected_Location = st.selectbox("Location", df['Location'].unique())
         c= df[df['Location'] == selected_Location][['Purchase Amount (USD)','Item Purchased','Gender','Category','Size','Season','Subscription Status']]
